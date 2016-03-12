6a10ba6137ee80283758d1cbd36a9882b77cec0f
mfrazi <muhammadfahrulrazi@gmail.com>
2016-03-12 19:59:25 +0700

    Add and seed roles table

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,7 +16,7 @@ class AuthController extends Controller
      */
     public function index()
     {
-        return view('form.PengumumanForm');
+        return view('form.PendaftarForm');
     }
 
     /**
---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'type' => 'dosen'
+        ]);
+        DB::table('roles')->insert([
+            'type' => 'admin'
+        ]);
+        DB::table('roles')->insert([
+            'type' => 'superuser'
+        ]);
+    }
+}
